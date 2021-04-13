96c162a38bbb7770f0f353076e3775892d3d0ce7
Kylory <72569852+Kylory@users.noreply.github.com>
2021-04-13 21:48:07 +0300

    added files

--- js/script.js
+++ js/script.js
@@ -1,46 +1,50 @@
 import gallery from './gallery-items.js';
 
 const galleryRef = document.querySelector('.js-gallery');
-const lightboxRef = document.querySelector('.js-lightbox')
+const lightboxRef = document.querySelector('.js-lightbox');
+const lightboxImageRef = document.querySelector('.lightbox__image');
+const modalCloseButtonRef = document.querySelector(
+  'button[data-action="close-lightbox"]',
+);
+const lightBoxOverlayRef = document.querySelector('.lightbox__overlay');
 
-{/* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>
-</li> */}
-{/* <li><a class="gallery__link" href=''><img class="gallery__image" src='' data-source='' alt=''/></a></li> */ }
-
-
-const makeListElement = ({preview, original, description}) => {
-    // return `<li><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></a></li>`;
-    return `<li><img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></li>`;
-};
-
-const elementList = gallery.map(makeListElement).join('');
-galleryRef.insertAdjacentHTML('beforeend', elementList)
-
-
-// Реалізація делегування на галереї ul.js-gallery і отримання url великого зображення.
-const onGalleryElementClick = (event) => {
-    // if (event.target.classList.contains("gallery__link")) {
-    //     lightboxRef.classList.add('is-open')
-    //     return (event.target.href)
-    //}
-    if (event.target.classList.contains("gallery__image")){
-        console.log('onGalleryElementClick');
-        console.log(event.target.src);
-        lightboxRef.classList.add('is-open')
-    }
+function makeElementMarkup({ preview, original, description }) {
+  return `<li><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></a></li>`;
 }
-galleryRef.addEventListener('click', onGalleryElementClick)
 
-// Відкриття модального вікна при натисканні на елементі галереї.
+function addNewElementsMarkup() {
+  galleryRef.insertAdjacentHTML(
+    'beforeend',
+    gallery.map(makeElementMarkup).join(''),
+  );
+}
+addNewElementsMarkup();
 
+function openModal() {
+  lightboxRef.classList.add('is-open');
+  window.addEventListener('keydown', onEscapePress);
+}
+
+function closeModal() {
+  lightboxImageRef.src = '';
+  lightboxRef.classList.remove('is-open');
+  window.removeEventListener('keydown', onEscapePress);
+}
+
+function onEscapePress(event) {
+  if (event.code === 'Escape') {
+    closeModal();
+  }
+}
+
+function onGalleryElementClick(event) {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    lightboxImageRef.src = event.target.getAttribute('data-source');
+    openModal();
+  }
+}
+
+galleryRef.addEventListener('click', onGalleryElementClick);
+modalCloseButtonRef.addEventListener('click', closeModal);
+lightBoxOverlayRef.addEventListener('click', closeModal);
