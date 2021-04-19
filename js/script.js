136a905e25d609a2b1d177ed941185986367824b
Kylory <72569852+Kylory@users.noreply.github.com>
2021-04-19 21:21:34 +0300

    refactored script

--- js/script.js
+++ js/script.js
@@ -33,10 +33,9 @@ function closeModal() {
 }
 
 function onLightBoxOverlayClick(event) {
-  if (event.target.nodeName === 'IMG') {
-    return;
+  if (event.target.nodeName !== 'IMG') {
+    closeModal();
   }
-  closeModal();
 }
 
 function onGalleryElementClick(event) {
